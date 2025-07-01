9b0247a484df9f9479bc2cbe405b009ec54df2af
soo <dlgustn96@gmail.com>
2025-07-01 20:12:17 +0900

    chore: 프로젝트 초기 세팅

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
   singleQuote: true,
   trailingComma: 'all',
+  arrowParens: 'always',
+  bracketSameLine: false,
 };
--- App.tsx
+++ App.tsx
@@ -1,20 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <Text style={styles.hello}>Hello World</Text>
     </View>
   );
 }
@@ -22,6 +11,13 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+  hello: {
+    color: '#F0997D',
+    fontSize: 70,
   },
 });
 
