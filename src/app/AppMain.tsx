f1a8097d710ff7a18d5712ae0b8bda5f87b1b691
soo <dlgustn96@gmail.com>
2025-07-05 13:29:15 +0900

    chore: tanstack query 설정

--- src/app/AppMain.tsx
+++ src/app/AppMain.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import RootStack from '@/app/RootStack';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const AppMain = () => {
   return (
-    <NavigationContainer>
-      <RootStack />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 };
 
