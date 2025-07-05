718eceb2278f714eff51c9ff90febcb407ce9289
soo <dlgustn96@gmail.com>
2025-07-06 04:38:29 +0900

    feat: AppHeader 및 DefaultLayout 컴포넌트 구현

---
+++ src/layouts/DefaultLayout.tsx
@@ -0,0 +1,47 @@
+import AppHeader from '@/components/AppHeader';
+import COLORS from '@/constants/colors';
+import { ReactNode } from 'react';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+
+type Props = {
+  children: ReactNode;
+  headerTitle: string;
+  onLeftPress?: () => void;
+  leftIcon?: ReactNode;
+  onRightPress?: () => void;
+  rightIcon?: ReactNode;
+};
+
+const DefaultLayout = ({
+  children,
+  headerTitle,
+  onLeftPress,
+  leftIcon,
+  onRightPress,
+  rightIcon,
+}: Props) => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <AppHeader
+        title={headerTitle}
+        onLeftPress={onLeftPress}
+        leftIcon={leftIcon}
+        onRightPress={onRightPress}
+        rightIcon={rightIcon}
+      />
+      <View style={styles.container}>{children}</View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: COLORS.WHITE,
+  },
+  container: {
+    flex: 1,
+  },
+});
+
+export default DefaultLayout;
